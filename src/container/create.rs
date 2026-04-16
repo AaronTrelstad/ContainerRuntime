488561f9d58c90f0c8b8c5295bdaecca0feb1f54
AaronTrelstad <trelstadaaron@gmail.com>
2026-04-15 20:40:53 -0500

    create

--- src/container/create.rs
+++ src/container/create.rs
@@ -1,9 +1,49 @@
+use nix::sched::{clone, CloneFlags};
+use nix::sys::wait::waitpid;
+use nix::unistd::{execvp, Pid};
+use std::ffi::CString;
+
 use crate::cli::CreateArgs;
 use crate::types::AnyError;
-use nix::sched::{clone, CloneFlags};
-use nix::unistd::{chroot, chdir};
-use nix::mount::{mount, MsFlags};
 
 pub fn create(args: CreateArgs) -> Result<(), AnyError> {
-    todo!()
+    /*
+    CLONE_NEWPID: This creates a new PID namespace, this isolates the child process so the PID is 1
+    CLONE_NEWNS: Copies the parents filesystem mounts to the child, so the child can mount/unmount without affecting other processes
+    CLONE_NEWUTS: Creates a new UTS (Unix Timesharing System) namespace for the child process, so it has its own hostname and domain name
+    CLONE_NEWIPC: Isolates IPC objects, prevents access of shared memory, semaphores or message queues
+    CLONE_NETNET: Isolates network interfaces, IP addresses, routing tables and socket listings
+
+    TODO
+    CLONE_NEWCGROUP
+    CLONE_NEWUSER
+    */
+    let flags = CloneFlags::CLONE_NEWPID 
+        | CloneFlags::CLONE_NEWNS
+        | CloneFlags::CLONE_NEWUTS
+        | CloneFlags::CLONE_NEWIPC
+        | CloneFlags::CLONE_NETNET;
+
+    let mut stack: Vec<u8> = vec![0u8; 1024 * 1024];
+
+    let child_pid = unsafe {
+        clone(
+            Box::new(|| match run_child() {
+                Ok(_) => 0,
+                Err(e) => {
+                    eprintln!("child error: {}", e);
+                    -1
+                }
+            }),
+            &mut stack,
+            flags,
+            None
+        )?
+    };
+
+    println!("Container PID: {}", child_pid);
+    waitpid(child, None)?;
+    println!("Container exited");
+
+    Ok(())
 }
