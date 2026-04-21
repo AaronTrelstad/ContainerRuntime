f42a42f0d66a5b2106104d8792efb7bb0cac0401
Asvantth Paranidharan <asvantth@gmail.com>
2026-04-20 21:32:51 -0500

    added to state.rs

--- src/container/state.rs
+++ src/container/state.rs
@@ -1,8 +1,40 @@
-use std::arch::aarch64::__crc32b;
-
+use std::fs;
+use std::path::{Path, PathBuf};
+use serde::{Serialize, Deserialize};
 use crate::cli::StateArgs;
 use crate::types::AnyError;
 
-pub fn state(args: StateArgs) -> Result<(), AnyError> {
-    todo!()
+const RUNTIME_DIR: &str = "/run/container-runtime";
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum ContainerStatus {
+    Created,
+    Running,
+    Stopped,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ContainerState {
+    pub id: String,
+    pub pid: u32,
+    pub status: ContainerStatus,
+    pub bundle: String,
+}
+
+impl ContainerState {
+    pub fn save(&self) -> Result<(), AnyError> {
+
+    }
+
+    pub fn load(container_id: &str) -> Result<Self, AnyError> {
+
+    }
+
+    pub fn delete(container_id: &str) -> Result<(), AnyError> {
+
+    }
+}
+
+fn container_dir(id: &str) -> PathBuf {
+    Path::new(RUNTIME_DIR).join(id)
 }
